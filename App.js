c636eb0a9d072b240a4c1eb25781006941dfeb3d
Joyce Querubino <joycequerubino5@gmail.com>
2021-03-06 23:02:50 -0300

    Add files

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Camera } from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null); //estado para ver se a permissão foi aceita ou não.
+  const [faces, setFaces] = useState([]);
+
+  const faceDetected = ({ faces }) => {
+    setFaces(faces);
+    console.log({ faces }); // ou (faces)
+  };
+
+  //utilizar um ciclio de vida de componentes: Quando ele abrir o app a primeira vez, dispare essa ação.
+  // será na primeira vez
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync(); //Pegue o estatus desse retorno
+      setHasPermission(status === "granted");
+    })();
+  }, []); //Com o arrey esta vazio, quando abrir o app vai ser executada
+
+  //Se for igual a null ou false é pq o usuário negou, então n pode usar a camera.
+  if (hasPermission !== true) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={{ flex: 1 }}
+        type="front"
+        onFacesDetected={faceDetected}
+        FaceDetectorSettings={{
+          mode: FaceDetector.Constants.Mode.fast,
+          detectLandmarks: FaceDetector.Constants.Landmarks.all,
+          runClassifications: FaceDetector.Constants.Classifications.all,
+          minDetectionInterval: 10000,
+          tracking: false,
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
